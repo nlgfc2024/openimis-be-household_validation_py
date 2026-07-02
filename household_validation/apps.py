9463190b1bcf4385e84e01510a008d527b4976ec
YutakaX17 <kademptr@gmail.com>
2026-07-02 20:35:08 +0200

    Add household validation permission config

--- household_validation/apps.py
+++ household_validation/apps.py
@@ -1,6 +1,74 @@
 from django.apps import AppConfig
 
+MODULE_NAME = "household_validation"
+
+RIGHT_HOUSEHOLD_VALIDATION_QUERY_EXPORT = 958001
+RIGHT_HOUSEHOLD_VALIDATION_UPLOAD = 958002
+RIGHT_HOUSEHOLD_VALIDATION_HISTORY = 958003
+RIGHT_HOUSEHOLD_VALIDATION_ERROR_REPORT = 958004
+
+RIGHT_GROUP_SEARCH = 180001
+RIGHT_GROUP_CREATE = 180002
+RIGHT_GROUP_UPDATE = 180003
+RIGHT_GROUP_DELETE = 180004
+
+HOUSEHOLD_VALIDATION_RIGHTS = [
+    RIGHT_HOUSEHOLD_VALIDATION_QUERY_EXPORT,
+    RIGHT_HOUSEHOLD_VALIDATION_UPLOAD,
+    RIGHT_HOUSEHOLD_VALIDATION_HISTORY,
+    RIGHT_HOUSEHOLD_VALIDATION_ERROR_REPORT,
+]
+
+GROUP_RIGHTS = [
+    RIGHT_GROUP_SEARCH,
+    RIGHT_GROUP_CREATE,
+    RIGHT_GROUP_UPDATE,
+    RIGHT_GROUP_DELETE,
+]
+
+DEFAULT_CONFIG = {
+    "gql_query_household_validation_rule_perms": [
+        str(RIGHT_HOUSEHOLD_VALIDATION_QUERY_EXPORT),
+    ],
+    "gql_mutation_generate_household_validation_list_perms": [
+        str(RIGHT_HOUSEHOLD_VALIDATION_QUERY_EXPORT),
+    ],
+    "gql_mutation_upload_household_validation_list_perms": [
+        str(RIGHT_HOUSEHOLD_VALIDATION_UPLOAD),
+    ],
+    "gql_query_household_validation_history_perms": [
+        str(RIGHT_HOUSEHOLD_VALIDATION_HISTORY),
+    ],
+    "gql_query_household_validation_error_report_perms": [
+        str(RIGHT_HOUSEHOLD_VALIDATION_ERROR_REPORT),
+    ],
+    "group_search_perms": [str(RIGHT_GROUP_SEARCH)],
+    "group_update_perms": [str(RIGHT_GROUP_UPDATE)],
+}
+
 
 class HouseholdValidationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'household_validation'
+    name = MODULE_NAME
+
+    gql_query_household_validation_rule_perms = None
+    gql_mutation_generate_household_validation_list_perms = None
+    gql_mutation_upload_household_validation_list_perms = None
+    gql_query_household_validation_history_perms = None
+    gql_query_household_validation_error_report_perms = None
+    group_search_perms = None
+    group_update_perms = None
+
+    @classmethod
+    def _load_config(cls, cfg):
+        """
+        Load config fields that match current AppConfig class fields.
+        """
+        for field in cfg:
+            if hasattr(cls, field):
+                setattr(cls, field, cfg[field])
+
+    def ready(self):
+        from core.models import ModuleConfiguration
+        cfg = ModuleConfiguration.get_or_default(self.name, DEFAULT_CONFIG)
+        self._load_config(cfg)
